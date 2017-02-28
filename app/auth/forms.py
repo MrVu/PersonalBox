671721c6d456ff8d36f505522324138dfeda3cf9
MrVu <chuvi0902@gmail.com>
2017-02-28 21:04:58 +1030

    user edit page

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,20 +1,34 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError
-from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo, DataRequired
 from ..models import User
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
-    password = PasswordField('Password', validators=[Required()])
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Log In')
 
 
 class RegisterForm(FlaskForm):
-    email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
     username = StringField('User name',
-                           validators=[Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Wrongformat')])
-    password = PasswordField('Password', validators=[Required(), EqualTo('password2', message="Not match")])
-    password2 = PasswordField('Comfirm password', validators=[Required()])
+                           validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_. ]*$', 0, 'Wrongformat')])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', message="Not match")])
+    password2 = PasswordField('Comfirm password', validators=[DataRequired()])
     submit = SubmitField('Register')
+
+
+class EditForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    username = StringField('User name',
+                           validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Wrongformat')])
+    submit = SubmitField('Submit')
+
+
+class ChangePassword(FlaskForm):
+    current_password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', message="Not match")])
+    password2 = PasswordField('Comfirm password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
--- app/auth/views.py
+++ app/auth/views.py
@@ -3,8 +3,8 @@ from .. import db
 from ..models import User
 from .. import functions
 from flask import render_template, redirect, url_for, request, flash
-from .forms import LoginForm, RegisterForm
-from flask_login import login_required, logout_user, login_user
+from .forms import LoginForm, RegisterForm, EditForm, ChangePassword
+from flask_login import login_required, logout_user, login_user, current_user
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -24,7 +24,7 @@ def login():
 def register():
     register_form = RegisterForm()
     while True:
-        random= functions.RandomID()
+        random = functions.RandomID()
         if User.query.filter_by(userkey=random).first() is not None:
             continue
         else:
@@ -32,7 +32,7 @@ def register():
     if register_form.validate_on_submit():
         user = User(email=register_form.email.data,
                     username=register_form.username.data,
-                    password=register_form.password.data, userkey= random)
+                    password=register_form.password.data, userkey=random)
         db.session.add(user)
         db.session.commit()
         if not functions.Installed(user.userkey):
@@ -41,9 +41,40 @@ def register():
     return render_template('register.html', register_form=register_form)
 
 
+@auth.route('/auth/profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    edit_form = EditForm()
+    if edit_form.validate_on_submit():
+        current_user.email = edit_form.email.data
+        current_user.username = edit_form.username.data
+        db.session.add(current_user)
+        db.session.commit()
+        return redirect(url_for('auth.edit_profile'))
+    edit_form.email.data = current_user.email
+    edit_form.username.data = current_user.username
+    return render_template('edit_profile.html', edit_form=edit_form)
+
+
+@auth.route('/auth/changepassword', methods=['GET', 'POST'])
+def change_password():
+    password_form = ChangePassword()
+    if password_form.validate_on_submit():
+        if current_user.verify_password(password_form.current_password.data):
+            current_user.password = password_form.password.data
+            db.session.add(current_user)
+            db.session.commit()
+            flash('Your Password is changed')
+            return redirect(url_for('auth.change_password'))
+        else:
+            flash('Your Password is incorrect')
+            return redirect(url_for('auth.change_password'))
+    return render_template('change_password.html', password_form=password_form)
+
+
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     flash('You have been logged out.')
-    return redirect(url_for('main.index'))
+    return redirect(url_for('auth.login'))
